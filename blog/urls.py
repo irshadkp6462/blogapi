278c9f713345662eb8ea3cb826e0cc13578252b3
irshad <irshadkuttiparambil@gmail.com>
2025-02-28 22:28:04 +0530

    api

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 
+from blog import views
+
 
 
 urlpatterns=[
 
-    
+    path("users/",views.UsercreationView.as_view()),
+
+    path("posts/",views.PostListCreateView.as_view()),
+
+    path('posts/<int:pk>/',views.PostRetrieveUpdateDestroyView.as_view()),
+
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
 
 # Create your views here.
+from blog.serializers import UserCreationSerializer,PostSerializer
+
+from rest_framework.generics import CreateAPIView,ListAPIView,RetrieveAPIView,UpdateAPIView,DestroyAPIView
+
+from blog.models import Post
+
+from rest_framework import authentication,permissions
+
+
+
+
+class UsercreationView(CreateAPIView):
+
+    serializer_class=UserCreationSerializer
+
+
+class PostListCreateView(ListAPIView,CreateAPIView):
+
+    authentication_classes=[authentication.BasicAuthentication]
+
+    permission_classes=[permissions.IsAuthenticated]
+
+    serializer_class=PostSerializer
+
+    queryset=Post.objects.all()
+
+    def perform_create(self, serializer):
+
+        return serializer.save(owner=self.request.user)
+    
+class PostRetrieveUpdateDestroyView(RetrieveAPIView,UpdateAPIView,DestroyAPIView):
+
+    serializer_class=PostSerializer
+
+    queryset=Post.objects.all()
+
+    authentication_classes=[authentication.BasicAuthentication]
+
+    permission_classes=[permissions.IsAuthenticated]
+
+
+
